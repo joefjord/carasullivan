7cb13fa079604dec51e6e4334d110e2fa54d5267
joe fjord <fordjoe@protonmail.com>
2023-06-08 10:48:51 -0500

    first commit

---
+++ mysql.sql
@@ -0,0 +1,3 @@
+-- cPanel mysql backup
+GRANT USAGE ON *.* TO 'carakxms'@'localhost' IDENTIFIED BY PASSWORD '*3E8827AFD00FDD3D2C09C4D996EB7371CAE8B0EE';
+GRANT ALL PRIVILEGES ON `carakxms\_%`.* TO 'carakxms'@'localhost';
